34537cf0f3945b73f29583d46543e5340a9ed77a
fQwQf <supertjz123@foxmail.com>
2025-06-23 17:14:21 +0800

    feat: 添加多种算法的运行命令及不同数据集的测试脚本

--- run.sh
+++ run.sh
@@ -27,3 +27,37 @@
 
 
 python test.py --cfp ./configs/CIFAR10_alpha0.1.yaml --algo OursV4
+
+# 运行FedAvg
+python test.py --cfp ./configs/CIFAR10_alpha0.1.yaml --algo FedAvg
+
+# 运行Ensemble
+python test.py --cfp ./configs/CIFAR10_alpha0.1.yaml --algo Ensemble
+
+# 运行OTFusion
+python test.py --cfp ./configs/CIFAR10_alpha0.1.yaml --algo OTFusion
+
+# 运行FedProto
+python test.py --cfp ./configs/CIFAR10_alpha0.1.yaml --algo FedProto
+
+# 运行FedETF
+python test.py --cfp ./configs/CIFAR10_alpha0.1.yaml --algo FedETF
+
+
+
+# 在CIFAR-100上运行
+python test.py --cfp ./configs/CIFAR100_alpha0.1.yaml --algo OursV4
+
+# 在SVHN上运行
+python test.py --cfp ./configs/SVHN_alpha0.1.yaml --algo OursV4
+
+# 在Tiny-ImageNet上运行
+python test.py --cfp ./configs/Tiny_alpha0.1.yaml --algo OursV4
+
+
+
+# CIFAR-10，不同alpha值
+python test.py --cfp ./configs/CIFAR10_alpha0.5.yaml --algo OursV4
+python test.py --cfp ./configs/CIFAR10_alpha0.3.yaml --algo OursV4
+python test.py --cfp ./configs/CIFAR10_alpha0.1.yaml --algo OursV4
+python test.py --cfp ./configs/CIFAR10_alpha0.05.yaml --algo OursV4
